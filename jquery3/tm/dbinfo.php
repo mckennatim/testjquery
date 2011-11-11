6dae0f1f996905564ff2c68aa71b6e1e565ff5b4
tim <tim@tim-Latitude-D510.(none)>
2011-11-11 12:44:50 -0500

    xub

--- jquery3/tm/dbinfo.php
+++ jquery3/tm/dbinfo.php
@@ -1,8 +1,8 @@
 <?php
 // defines database connection data
 define('DB_HOST', 'localhost');
-define('DB_USER', 'pathbost_tim');
-define('DB_PASSWORD', 'nji9ol');
+define('DB_USER', 'pathbost_user');
+define('DB_PASSWORD', 'user');
 define('DB_DATABASE', 'pathbost_ob'); 
 // defines the number of visible rows in grid
 ?>
