a88be5b18cf992c0b867b4c8992291abd26c2751
tim <tim@tim-Latitude-D510.(none)>
2011-11-11 23:08:56 -0500

    done testing

--- jquery3/DeleteData.php
+++ jquery3/DeleteData.php
@@ -1,1 +1,21 @@
-ok
+<?php
+session_start();
+include_once('tm/dbinfo.php');
+require_once('tm/FirePHP.class.php');
+require_once('tm/fb.php');
+ob_start(); //gotta have this
+fb('how are you today');
+  
+
+  //DeleteData.php
+  $id = $_REQUEST['id'] ;
+
+  /* Delete a record by id */ 
+mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) or die("can't connect");
+mysql_select_db (DB_DATABASE) or die("db unavailable");
+
+$sql= "DELETE FROM roles 
+WHERE rid = '$id'";
+mysql_query($sql) or die("Dead inserting");
+
+  echo "ok";
